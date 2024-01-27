3b687b8f7295d71ac168e45a8b79f79e44f1a82d
Meryem Marasli <mgmmeryem@gmail.com>
2024-01-27 14:41:59 -0500

    updating files

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,49 +1,16 @@
 <template>
- <GMapMap
-      :center="center"
-      :zoom="7"
-      map-type-id="terrain"
-      style="width: 500px; height: 300px"
-  >
-    <GMapCluster>
-      <GMapMarker
-          :key="index"
-          v-for="(m, index) in markers"
-          :position="m.position"
-          :clickable="true"
-          :draggable="true"
-          @click="center=m.position"
-      />
-    </GMapCluster>
-  </GMapMap>
-
-
- 
-
-
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="heyyyy tasneem, berfin, and zeana!"/>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  data() {
-    return {
-      center: {lat: 51.093048, lng: 6.842120},
-      markers: [
-        {
-          position: {
-            lat: 51.093048, lng: 6.842120
-          },
-        }
-        , // Along list of clusters
-      ]
-    }
+  components: {
+    HelloWorld
   }
-  // components: {
-  //   HelloWorld
-  // }
 }
 </script>
 
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,14 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/tailwind.css'
-import VueGoogleMaps from '@fawmi/vue-google-maps'
+
+createApp(App).mount('#app')
 
 
-const app = createApp(App);
-app.use(VueGoogleMaps, {
-    load: {
-        key: 'AIzaSyBSVJGbH7Xwi-RFeogw4ish2fPwsvt23Ic',
-    },
-}).mount('#app')
+
+// const app = createApp(App);
+// app.use(VueGoogleMaps, {
+//     load: {
+//         key: 'AIzaSyBSVJGbH7Xwi-RFeogw4ish2fPwsvt23Ic',
+//     },
+// }).mount('#app')
+
+// import VueGoogleMaps from '@fawmi/vue-google-maps'
+
 
 
